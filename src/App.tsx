7cc5aff08b672e22901b1b73d9491efba58ac356
Phaired <65019388+Phaired@users.noreply.github.com>
2023-09-11 11:04:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import './App.css'
+import {useEffect, useState} from "react";
+import {Line} from "rc-progress";
+
+function App() {
+
+    const endDate = new Date()
+    endDate.setHours(22)
+    endDate.setMinutes(59)
+    endDate.setSeconds(59)
+
+    endDate.setFullYear(2025)
+    endDate.setMonth(2)
+    endDate.setDate(28)
+
+    const startDate = new Date()
+    startDate.setHours(8)
+    startDate.setMinutes(0)
+    startDate.setSeconds(0)
+
+    startDate.setFullYear(2023)
+    startDate.setMonth(8)
+    startDate.setDate(4)
+
+
+
+    const [d, setD] = useState<number>(0)
+    const [h, setH] = useState<number>(0)
+    const [m, setM] = useState<number>(0)
+    const [s, setS] = useState<number>(0)
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const remainingTime = new Date(endDate.getTime() - Date.now());
+
+            const days = Math.floor(remainingTime.getTime() / 1000 / 60 / 60 / 24);
+            const hours = Math.floor((remainingTime.getTime() / 1000 / 60 / 60) % 24);
+            const minutes = Math.floor((remainingTime.getTime() / 1000 / 60) % 60);
+            const seconds = Math.floor((remainingTime.getTime() / 1000) % 60);
+
+            // Mettre à jour les états
+            setD(days);
+            setH(hours);
+            setM(minutes);
+            setS(seconds);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [endDate]);
+
+    const pluralize = (count: number, unit: string) => (count < 2 ? unit : unit + 's');
+
+
+    return (
+        <>
+
+            {d !== 0 && (
+            <div style={{width: "100%"}}>
+                <p>
+                    Encore {d} {pluralize(d, 'jour')} {h} {pluralize(h, 'heure')} {m} {pluralize(m, 'minute')} {s} {pluralize(s, 'seconde')}.
+                </p>
+                <p>
+                    Nous finissons le 28 mars 2025 🥳🥳🥳
+                </p>
+                <span style={{fontSize :"14px",
+                                display: "flex",
+                                alignItems: "center",
+                    justifyContent: "center",
+                    gap: "10px"
+                }}>
+
+
+                <Line style={{width: "clamp(200px, 20%, 30vw)"}} percent={((Date.now() - startDate.getTime()) / (endDate.getTime() - startDate.getTime())*100)} strokeWidth={2} strokeColor="green" />
+                    { ((Date.now() - startDate.getTime()) / (endDate.getTime() - startDate.getTime())*100).toPrecision(5)} %
+                </span>
+                </div>)}
+        </>
+
+  )
+}
+
+export default App
